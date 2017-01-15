787f9212d415f4c7453a8d8150e60962664b0652
Jack Palevich <jack@fuji>
2017-01-15 12:51:36 -0800

    It now filters some extra carp

--- DetectingSoundFrequency/NoteToMessage.m
+++ DetectingSoundFrequency/NoteToMessage.m
@@ -11,10 +11,15 @@
 @implementation NoteToMessage{
     Float32 _C0;
     NSArray * _name;
+    NSString * _noteString;
+    NSString * _previousNote;
+    BOOL _recordNotes;
 }
 -(instancetype) init{
     self = [super init];
     if(self){
+        _noteString = @"";
+        _previousNote = @"";
         Float32 A4 = 440;
         _C0 = A4 * powf(2, -4.75);
         _name = @[@"C", @"C#", @"D", @"D#", @"E", @"F", @"F#", @"G", @"G#", @"A", @"A#", @"B"];
@@ -23,11 +28,41 @@
 }
 - (NSString *) messageForFreq: (Float32) freq power: (Float32) power
 {
+    
     if(power < 1e-6)
     {
-        return @"silence";
+        return _noteString;
     }
-    return [self pitch:freq];
+    NSString * tempNote = [self pitch:freq];
+    if(_previousNote == tempNote)
+    {
+        return _noteString;
+    }
+    if([tempNote containsString:@"#"] || [tempNote isEqualToString:@"F0"] )
+    {
+        return _noteString;
+    }
+    if(![tempNote containsString:@"4"] && ![tempNote containsString:@"5"])
+    {
+        return _noteString;
+    }
+    if([tempNote isEqualToString:@"A5"])
+    {
+        _recordNotes = YES;
+        _noteString = @"";
+        return _noteString;
+    }
+    if([tempNote isEqualToString:@"B5"])
+    {
+        _recordNotes = NO;
+    }
+    if(!_recordNotes)
+    {
+        return _noteString;
+    }
+    _previousNote = [self pitch:freq];
+    _noteString =  [_noteString stringByAppendingString:[self pitch: freq]];
+    return _noteString;
 }
 
 
